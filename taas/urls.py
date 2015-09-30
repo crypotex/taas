50b7d3dc1f932ba8162cd0e787e650384a670bff
Annika Laumets <laumets.annika@gmail.com>
2015-09-30 20:37:31 +0300

    Added basic design (not dynamic) and translations for the nav menu. Also pictures for the nav. Issue #12

--- taas/urls.py
+++ taas/urls.py
@@ -7,7 +7,8 @@ admin.site.site_header = _('TAAS administration')
 
 urlpatterns = patterns(
     '',
-    url(r'^', TemplateView.as_view(template_name='base.html')),
+    # Kui järgmine rida pole välja kommenteeritud, siis ta näitab iga aadressiga ainult base.htmli
+    #url(r'^', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('taas.user.urls')),
 )
