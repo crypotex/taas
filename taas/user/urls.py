5afaebe3056c0dd0efcd2bf8b7b4738f39dedbdd
crypotex <b02166@ut.ee>
2015-10-01 16:34:14 +0300

    Revert "Remove username from User model (feature/36)"

--- taas/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^register/$', 'taas.user.views.register_user', name='user_registration_form'),
-    url(r'^register/success/$', TemplateView.as_view(template_name='registration_success.html')),
-]
