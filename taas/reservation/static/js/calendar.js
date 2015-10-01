5c8bdfac9d8e0cca54150380e1d9176938ccdb13
Annika Laumets <laumets.annika@gmail.com>
2015-10-01 14:28:14 +0300

    Fullcalendar has 3 columns and you can drap&drop events. Issue #16

--- taas/reservation/static/js/calendar.js
+++ taas/reservation/static/js/calendar.js
@@ -4,13 +4,31 @@ $(document).ready(function() {
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
-        defaultView: 'agendaWeek',
+        views: {
+            multiColAgendaWeek: {
+                type: 'multiColAgenda',
+                duration: { weeks: 1 },
+                numColumns: 3
+            }
+        },
+        defaultView: 'multiColAgendaWeek',
         firstDay: 1,
         slotDuration: '01:00:00',
         slotEventOverlap: false,
         minTime: '08:00:00',
         maxTime: '22:00:00',
-        contentHeight: 400
-    })
-
+        contentHeight: 450,
+        editable: true,
+         header: {
+            left: 'prev',
+            center: 'title',
+            right: 'next'
+        },
+        events: [{
+            title: 'Some event',
+            start: $.fullCalendar.moment('2015-10-01T12:00:00'), // now
+            end: $.fullCalendar.moment('2015-10-01T14:00:00'),
+            column: 1
+        }]
+    });
 });
