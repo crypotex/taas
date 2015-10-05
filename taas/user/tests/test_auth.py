83c81239ce3d4ffa886bab491c5381e44a5a6870
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-10-05 11:42:18 +0300

    Fix user and authentication tests
    Issue #11

--- taas/user/tests/test_auth.py
+++ taas/user/tests/test_auth.py
@@ -1,48 +1,39 @@
 from http import client as http_client
 
-from django.test import TransactionTestCase
-from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
-from django.contrib.auth.forms import AuthenticationForm
-
+from django.test import TestCase
 
 from .factories import UserFactory
 
 
-class UserLoginTest(TransactionTestCase):
+class UserLoginTest(TestCase):
     def setUp(self):
         self.user = UserFactory(is_active=True)
+        self.login_data = {'username': self.user.email, 'password': 'isherenow'}
         self.login_url = UserFactory.get_login_url()
 
     def test_user_can_access_login(self):
         response = self.client.get(self.login_url)
-        self.assertEqual(response.status_code, http_client.OK, 'User cannot log in.')
+        self.assertEqual(response.status_code, http_client.OK,
+                         'User cannot log in with valid data.')
 
     def test_user_can_login_with_valid_arguments(self):
-        data = {'username': self.user.email, 'password': self.user.password}
-        login_form = AuthenticationForm(data)
-        # self.assertEqual(response.status_code, http_client.OK, 'User cannot login.')
-        self.assertTrue(login_form.is_valid())
+        response = self.client.post(self.login_url, self.login_data)
+        self.assertRedirects(response,
+                             expected_url=reverse('homepage'),
+                             status_code=http_client.FOUND,
+                             target_status_code=http_client.OK)
+        self.assertEqual(int(self.client.session['_auth_user_id']), self.user.pk,
+                         'User cannot login with valid data.')
 
-    ### Have to change these two tests
     def test_user_cannot_login_with_invalid_username(self):
-        self.user.email = "vale@email.com"
-        login_form = AuthenticationForm(data=UserFactory.get_form_data())
-        self.assertFalse(login_form.is_valid())
-        self.assertEqual(login_form.errors['__all__'][0],
-                         _('Please enter a correct username and password. Note that both fields may be case-sensitive.'))
+        self.login_data['username'] = 'vale@email.com'
+        response = self.client.post(self.login_url, self.login_data)
+        self.assertEqual(response.status_code, http_client.OK,
+                         'User can login with invalid email.')
 
     def test_user_cannot_login_with_invalid_password(self):
-        response = self.client.post(self.login_url, self.inv_user2)
-        ##self.assertNotEqual(response.status_code, http_client.OK, 'User can login with invalid password.')
-
-    #Helper methods
-    def get_user_dict(self, user):
-        return {
-            'email': user.email,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'phone_number': user.phone_number,
-            'password1': 'isherenow',
-            'password2': 'isherenow'
-        }
+        self.login_data['password'] = 'invalid'
+        response = self.client.post(self.login_url, self.login_data)
+        self.assertEqual(response.status_code, http_client.OK,
+                         'User can login with invalid password.')
