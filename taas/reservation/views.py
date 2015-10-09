1f1c092d2abac0ee62e22ee781aef48c3f2db65d
crypotex <andre.tattar@ut.ee>
2015-10-10 01:14:20 +0300

    Authorization for make reservation page is now working. You can now make reservations in admin page and html page. User/admin cannot make an incorrect reservation.
    Issue #20

--- taas/reservation/views.py
+++ taas/reservation/views.py
@@ -5,7 +5,7 @@ from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.decorators import login_required
-
+from django.http.request import QueryDict
 from django.http import Http404
 
 from django.http import HttpResponseRedirect
@@ -32,25 +32,21 @@ class ReservationView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     login_url = reverse_lazy("user_login_form")
     redirect_authenticated_users = True
 
+
     def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-        if not request.user.is_authenticated():
-            return HttpResponseRedirect
-        tuser = request.user
+        p_user = request.user
+
         ## For post method security
-        if not tuser.is_authenticated:
+        if not p_user.is_authenticated:
             raise Http404
 
-        if form.is_valid():
-            # <process form cleaned data>
-            return HttpResponseRedirect('/success/')
-        raise Http404
-        ##return render(request, self.template_name, {'form': form})
+        post_data = QueryDict('', mutable=True)
+        post_data.update(request.POST)
+        post_data.update({'user':str(p_user.id)})
 
-    '''
-    Pole vaja hetkel
-    def form_valid(self, form):
-        logger.info('Reservation made by %s for %s has been successfully registered.'
-                    % form.cleaned_data.get('user'), form.cleaned_data.get('start_date'))
-        return super(ReservationView, self).form_valid(form)
-    '''
+        form = self.form_class(post_data)
+        if form.is_valid():
+            reservation = form.save()
+            return HttpResponseRedirect(self.success_url)
+        else:
+            raise Http404
