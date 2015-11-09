f13d3d1be6fd5d26246725f5e3e8de8c55cfda15
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-11-09 03:12:31 +0200

    Fix binding address
    Issue #91

--- taas/user/tests/test_frontend_user_update.py
+++ taas/user/tests/test_frontend_user_update.py
@@ -1,6 +1,11 @@
+import os
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from taas.user.tests.factories import UserFactory
+
+
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8000-8010,8080,9200-9300'
 
 
 class UserUpdateTest(StaticLiveServerTestCase):
