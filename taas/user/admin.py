837ba3ed5ae8751f4a6176647da9a2276067b1f0
crypotex <andre.tattar@ut.ee>
2015-12-03 15:00:36 +0200

    Made pin non-editable on admin page.
    Issue #120

--- taas/user/admin.py
+++ taas/user/admin.py
@@ -22,6 +22,7 @@ class UserAdmin(auth_admin.UserAdmin):
             'fields': ('first_name', 'last_name', 'email', 'phone_number', 'password1', 'password2'),
         }),
     )
+    readonly_fields = ('pin',)
     list_display = ('email', 'first_name', 'last_name', 'is_active')
     list_filter = ('is_staff', 'is_active')
     search_fields = ('first_name', 'last_name')
