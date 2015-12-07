c1a6f20a7b1985534486c7a478dff145953229de
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2015-12-07 05:07:52 +0200

    Add live test module env variable
    Issue #31

--- taas/user/tests/test_frontend_registration.py
+++ taas/user/tests/test_frontend_registration.py
@@ -8,6 +8,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from taas.user.tests.factories import UserFactory
 
 
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = "localhost:8000-8010,8080,9200-9300"
+
+
 class UserRegistrationTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
